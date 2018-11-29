145c3d3b6636990b1787dcf1220b47d9c3bbd126
Larry512 <jaredpotter1@gmail.com>
2018-11-29 13:58:51 -0700

    Init Commit

---
+++ ssr/components/BrowserOnlyComponent.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h1>Browser Only Component - Current URL: {{ currentURL }}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            currentURL: ''
+        };
+    },
+    created() {
+        let url = document.URL;
+
+        this.url = url;
+    },
+    // mounted() {
+    //     let url = document.URL;
+
+    //     this.url = url;
+    // },
+}
+</script>
---
+++ ssr/pages/index.vue
@@ -0,0 +1,90 @@
+<template>
+  <section class="container">
+    <div>
+      <logo/>
+      <h1 class="title">
+        ssr
+      </h1>
+      <h2 class="subtitle">
+        My slick Nuxt.js project
+      </h2>
+      <h2 class="subtitle">
+        Current URL from Document: {{ currentURL }}
+      </h2>
+      <no-ssr>
+        <BrowserOnlyComponent/>
+      </no-ssr>
+      <div class="links">
+        <a
+          href="https://nuxtjs.org/"
+          target="_blank"
+          class="button--green">Documentation</a>
+        <a
+          href="https://github.com/nuxt/nuxt.js"
+          target="_blank"
+          class="button--grey">GitHub</a>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+import BrowserOnlyComponent from '~/components/BrowserOnlyComponent.vue'
+
+export default {
+  components: {
+    Logo, BrowserOnlyComponent
+  },
+  data() {
+    return {
+      currentURL: ''
+    };
+  },
+  created () {
+    // let url = document.URL;
+    
+    // this.currentURL = url;
+  },
+  mounted() {
+    let url = document.URL;
+    this.currentURL = url;
+
+    let window = window;
+    debugger;
+  }
+}
+</script>
+
+<style>
+
+.container {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
